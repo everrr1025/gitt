70647824b79a4284c87705507bfa863d9a28072d
everrr <2037894@qq.com>
2024-09-12 10:31:08 +0800

    add js file

---
+++ func.js
@@ -0,0 +1,12 @@
+
+const addLine = (a,b)=>{
+	let x = 'haha my name is Honda haha'
+	return a+b;
+}
+
+let t = 'who r u , my name is Hongda';
+let x = 'your name is everrr'
+let a = 'haha isRussell'
+
+
+
